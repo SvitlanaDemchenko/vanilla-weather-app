04768ce628ff47165b6f8c1be988830498eb11c2
SvitlanaDemchenko <kotenych@gmail.com>
2022-07-26 15:03:01 +0300

    added search engine

--- src/app.js
+++ src/app.js
@@ -40,12 +40,19 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute(
-    "alt",
-    response.data.weather[0].description);
-  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "74da28a6616154ed42cc3e9e410de59a";
-let city = "Moscow";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "74da28a6616154ed42cc3e9e410de59a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
