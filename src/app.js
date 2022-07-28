921d0f2142229a400b4005c40a3195b58683791d
SvitlanaDemchenko <kotenych@gmail.com>
2022-07-28 14:07:40 +0300

    created forecast grid

--- src/app.js
+++ src/app.js
@@ -22,6 +22,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <img
+                    src="http://openweathermap.org/img/wn/02d@2x.png"
+                    alt=""
+                    width="46px"
+                  />
+                  <div class="weather-forecast-temp">
+                    <span class="weather-forecast-temp-max">18°</span>
+                    <span class="weather-forecast-temp-min">12°</span>
+                  </div>
+                </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +106,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 search("New York");
+displayForecast();
