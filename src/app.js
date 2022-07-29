9eaea92690b44f72f3a301a08fcf054731dc703f
SvitlanaDemchenko <kotenych@gmail.com>
2022-07-29 15:31:40 +0300

    improved css

--- src/app.js
+++ src/app.js
@@ -63,7 +63,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5c2ff80778e5c1edc5c59ec4c69ffecc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
